b22bbccdec253e65b5c6ed6e57ccd802ecfdef5c
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-23 19:31:38 -0400

    Minor.

--- src/org/mtransit/parser/ca_strathcona_county_transit_bus/StrathconaCountyTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_strathcona_county_transit_bus/StrathconaCountyTransitBusAgencyTools.java
@@ -14,7 +14,6 @@ import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
-import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GStopTime;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.gtfs.data.GTripStop;
@@ -22,7 +21,6 @@ import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MSpec;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
 // https://data.strathcona.ca/
 // https://data.strathcona.ca/Transportation/Strathcona-County-Transit-Bus-Schedule-GFTS-Data-F/cvta-prr6
