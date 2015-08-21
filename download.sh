fa640e604c86950deadbb2af46193c77c78c87ad
Mathieu Méa <mathieu.mea@gmail.com>
2015-08-21 09:55:29 -0400

    Limit download timeout to 60 seconds and 6 retries (instead of the default 900 seconds timeout and 20 retries)...

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ">> Downloading..."
-wget --header="User-Agent: MonTransit" -i input_url -O input/gtfs.zip
+wget --header="User-Agent: MonTransit" --timeout=60 --tries=6 -i input_url -O input/gtfs.zip
 echo ">> Downloading... DONE"
